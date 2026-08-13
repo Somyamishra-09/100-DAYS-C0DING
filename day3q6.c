ddc07d0bd3603b20330dd210afcafa2a3205af37
Varsha Nashine <somya.37289@stu.upes.ac.in>
2026-08-14 00:52:06 +0530

    Add Day 3 Q6

--- day3q6.c
+++ day3q6.c
@@ -15,3 +15,11 @@ int main() {
 
     return 0;
 }
+/*
+PS C:\MinGW\bin> & C:\Users\shivo\OneDrive\Desktop\programing\day3q6.exe
+Enter two numbers: 3 5
+After swapping: 5 3
+PS C:\MinGW\bin> & C:\Users\shivo\OneDrive\Desktop\programing\day3q6.exe
+Enter two numbers: -1 1
+After swapping: 1 -1
+PS C:\MinGW\bin> */
