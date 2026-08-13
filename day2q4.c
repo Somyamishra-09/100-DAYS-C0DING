37a7e1c21e59c82f02cd1e94a861b908c018739e
Varsha Nashine <somya.37289@stu.upes.ac.in>
2026-08-14 00:14:12 +0530

    Update Day 2 Q4

--- day2q4.c
+++ day2q4.c
@@ -15,3 +15,11 @@ int main() {
 
     return 0;
 }
+/*
+PS C:\MinGW\bin> C:\Users\shivo\OneDrive\Desktop\programing\day2q4.exe
+Enter the radius of the circle: 7
+Area=153.94, Circumference=43.98
+PS C:\MinGW\bin> C:\Users\shivo\OneDrive\Desktop\programing\day2q4.exe
+Enter the radius of the circle: 3
+Area=28.27, Circumference=18.85
+PS C:\MinGW\bin> */
