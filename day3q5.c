a5dd97fa3331c3a9a31f8a6f31ad3676a397dab3
Varsha Nashine <somya.37289@stu.upes.ac.in>
2026-08-14 00:44:34 +0530

    Update Day 3 Q5

--- day3q5.c
+++ day3q5.c
@@ -13,3 +13,12 @@ int main() {
 
     return 0;
 }
+/*
+PS C:\MinGW\bin> C:\Users\shivo\OneDrive\Desktop\programing\day3q5.exe
+Enter temperature in Celsius: 0
+Temperature in Fahrenheit: 32.00
+PS C:\MinGW\bin> C:\Users\shivo\OneDrive\Desktop\programing\day3q5.exe
+Enter temperature in Celsius: 100
+Temperature in Fahrenheit: 212.00
+PS C:\MinGW\bin> 
+*/
